5710526f5f2a28e2e77074792abd3a8d474fbd32
Natta Wang <xenogew@gmail.com>
2025-06-08 20:12:29 +0900

    initial commit

---
+++ spring-boot/src/test/java/blog/natta/santa/cors/CorsApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.natta.santa.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
