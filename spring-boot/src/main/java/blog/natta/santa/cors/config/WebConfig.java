a71175b2416c5ce6c838d6a347d7c448691767d6
Natta Wang <xenogew@gmail.com>
2025-06-09 01:00:19 +0900

    changes WebConfig class

--- spring-boot/src/main/java/blog/natta/santa/cors/config/WebConfig.java
+++ spring-boot/src/main/java/blog/natta/santa/cors/config/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:5173");
     }
 }
--- spring-boot/src/test/java/blog/natta/santa/cors/config/WebConfigTest.java
+++ spring-boot/src/test/java/blog/natta/santa/cors/config/WebConfigTest.java
@@ -1,7 +1,19 @@
 package blog.natta.santa.cors.config;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 class WebConfigTest {
+    private final WebConfig webConfig = new WebConfig();
 
+    @Test
+    void When_AddCorsMappings_Then_ReturnSuccess() {
+        // Arrange
+        final CorsRegistry corsRegistry = new CorsRegistry();
+
+        // Act & Assert
+        assertDoesNotThrow(() -> webConfig.addCorsMappings(corsRegistry));
+    }
 }
