11a7d847b14411d65cb422ac2eeb65836aaf284f
Natta Wang <xenogew@gmail.com>
2025-11-27 00:58:04 +0900

    get rid @CrossOrigin from Deliver endpoint as it is sample case of Global Config

--- spring-boot/src/main/java/blog/natta/santa/cors/delivery/endpoint/DeliveryController.java
+++ spring-boot/src/main/java/blog/natta/santa/cors/delivery/endpoint/DeliveryController.java
@@ -1,7 +1,6 @@
 package blog.natta.santa.cors.delivery.endpoint;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ import blog.natta.santa.cors.delivery.response.TrackingResultResponse;
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE)
 public class DeliveryController {
-  @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping(value = "/status/{trackingId}")
     public TrackingResultResponse trackDelivery(@PathVariable String trackingId) {
         return TrackingResultResponse.builder()
